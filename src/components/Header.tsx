df455407e48f08c6cfd5aeb26720ad373bac1241
Kieran <krkearney11@outlook.com>
2026-04-11 14:07:27 +0100

    l

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="header">
       <nav>
-        <h1>Hello, Vistor</h1>
+        <h1>Hello, </h1>
       </nav>
     </header>
   );
